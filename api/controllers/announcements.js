068ce9135565aa00ac81fa64321ced732187d3dd
tharpon0207 <tharpon0207@gmail.com>
2024-10-26 11:38:53 -0400

    announcements

--- api/controllers/announcements.js
+++ api/controllers/announcements.js
@@ -9,42 +9,42 @@ let Op = Sequelize.Op;
 module.exports = { addAnnouncement, updateAnnouncement, getAllAnnouncements }
 
 function addAnnouncement(req, res, next) {
-  let event = req.swagger.params.event.value;
+  let announcement = req.swagger.params.event.value;
   //req.body.member_id
-  let newEvent = {
-    date: event.date,
-    title: event.title,
+  let newAnnouncement = {
+    date: announcement.date,
+    title: announcement.title,
   };
-  if (event.start !== null) {
-    newEvent.start = event.start;
+  if (announcement.start !== null) {
+    newAnnouncement.start = announcement.start;
   };
-  if (event.end !== null) {
-    newEvent.end = event.end;
+  if (announcement.end !== null) {
+    newAnnouncement.end = announcement.end;
   };
-  if (event.description !== null) {
-    newEvent.description = event.description;
+  if (announcement.description !== null) {
+    newAnnouncement.description = announcement.description;
   };
-  if (event.publish !== null) {
-    newEvent.publish = event.publish;
+  if (announcement.publish !== null) {
+    newAnnouncement.publish = announcement.publish;
   };
-  if (event.status !== null) {
-    newEvent.status = event.status;
+  if (announcement.status !== null) {
+    newAnnouncement.status = announcement.status;
   };
-  //console.log(newEvent);
-  Announcements.create(newEvent)
+  //console.log(newAnnouncement);
+  Announcements.create(newAnnouncement)
     .then(data => {
       if (data !== null) {
         Announcements.findOne({
-          where: { title: event.title, date: Sequelize.fn('DATE', event.date) },
+          where: { title: announcement.title, date: Sequelize.fn('DATE', announcement.date) },
           order: [
             ['id', 'DESC'],
             ['created_at', 'DESC'],
           ],
-          attributes: ['id', [Sequelize.fn('MONTHNAME', Sequelize.col('date')), 'month'], [Sequelize.fn('DAY', Sequelize.col('date')), 'day'], 'start', 'end', 'title', ['description', 'desc'], 'publish', 'status']
+          attributes: ['id', [Sequelize.fn('CONCAT_WS', ',', Sequelize.fn('MONTHNAME', Sequelize.col('date')), Sequelize.fn('DAY', Sequelize.col('date')), Sequelize.fn('YEAR', Sequelize.col('date'))), 'date'], 'title', ['description', 'desc'], 'publish', 'status']
         })
           .then(data => {
             if (data != null) {
-              res.json({ error: false, event: data });
+              res.json({ error: false, announcement: data });
             } else {
               res.status(500).json({ error: true, message: "some error occured while processing the request" });
             }
@@ -61,44 +61,38 @@ function addAnnouncement(req, res, next) {
     );
 }
 function updateAnnouncement(req, res, next) {
-  let event = req.swagger.params.event.value;
+  let announcement = req.swagger.params.event.value;
   //req.body.member_id
-  let newEvent = {
-    date: event.date,
-    title: event.title,
+  let newAnnouncement = {
+    date: announcement.date,
+    title: announcement.title,
   };
-  if (event.start !== null) {
-    newEvent.start = event.start;
+  if (announcement.description !== null) {
+    newAnnouncement.description = announcement.description;
   };
-  if (event.end !== null) {
-    newEvent.end = event.end;
+  if (announcement.publish !== null) {
+    newAnnouncement.publish = announcement.publish;
   };
-  if (event.description !== null) {
-    newEvent.description = event.description;
+  if (announcement.status !== null) {
+    newAnnouncement.status = announcement.status;
   };
-  if (event.publish !== null) {
-    newEvent.publish = event.publish;
-  };
-  if (event.status !== null) {
-    newEvent.status = event.status;
-  };
-  //console.log(newEvent);
-  Announcements.update(newEvent,
+  //console.log(newAnnouncement);
+  Announcements.update(newAnnouncement,
     {
       where: {
-        id: event.id
+        id: announcement.id
       },
     },
   ).then(data => {
     if (data !== null) {
       Announcements.findOne({
-        where: { id: event.id },
-        attributes: ['id', [Sequelize.fn('MONTHNAME', Sequelize.col('date')), 'month'], [Sequelize.fn('DAY', Sequelize.col('date')), 'day'], 'start', 'end', 'title', ['description', 'desc'], 'publish', 'status']
+        where: { id: announcement.id },
+        attributes: ['id', [Sequelize.fn('CONCAT_WS', ',', Sequelize.fn('MONTHNAME', Sequelize.col('date')), Sequelize.fn('DAY', Sequelize.col('date')), Sequelize.fn('YEAR', Sequelize.col('date'))), 'date'], 'title', ['description', 'desc'], 'publish', 'status']
       })
         .then(data => {
           if (data != null) {
             console.log(data);
-            res.json({ error: false, event: data });
+            res.json({ error: false, announcement: data });
           } else {
             res.status(500).json({ error: true, message: "some error occured while processing the request" });
           }
@@ -122,13 +116,14 @@ function getAllAnnouncements(req, res, next) {
 
 
   Announcements.findAndCountAll({
-    attributes: ['id', [Sequelize.fn('MONTHNAME', Sequelize.col('date')), 'month'], [Sequelize.fn('DAY', Sequelize.col('date')), 'day'], 'start', 'end', 'title', ['description', 'desc'], 'publish', 'status'], offset: offset,
+    attributes: ['id', [Sequelize.fn('CONCAT_WS', ',', Sequelize.fn('MONTHNAME', Sequelize.col('date')), Sequelize.fn('DAY', Sequelize.col('date')), Sequelize.fn('YEAR', Sequelize.col('date'))), 'date'], 'title', ['description', 'desc'], 'publish', 'status'],
+    offset: offset,
     limit: limit,
   })
     .then(data => {
       if (data != null) {
         let total = (data.count % limit == 0) ? data.count / limit : parseInt(data.count / limit) + 1;
-        res.json({ error: false, count: data.count, total_page: total, offset: offset, limit: limit, Announcements: data.rows });
+        res.json({ error: false, count: data.count, total_page: total, offset: offset, limit: limit, announcements: data.rows });
       } else {
         res.status(204).send();
       }
